1e822436dff9e88b76166c7e0893cdd25f5af8e3
EthanStark <n_f.s@163.com>
2020-04-10 15:24:58 +0800

    hello

--- src/main/java/com/caoc/test/parameterizedType/ParameterizedBean.java
+++ src/main/java/com/caoc/test/parameterizedType/ParameterizedBean.java
@@ -1,27 +1,49 @@
 package com.caoc.test.parameterizedType;
 
-import java.lang.reflect.Field;
-import java.lang.reflect.ParameterizedType;
-import java.lang.reflect.Type;
+import org.apache.commons.lang3.reflect.FieldUtils;
+import org.apache.commons.lang3.reflect.MethodUtils;
+
+import java.lang.reflect.*;
+import java.sql.SQLOutput;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
-public class ParameterizedBean {
+public class ParameterizedBean extends ParentFields {
 	List<String> list1;
 	List list2;
 	Map<String, Long> map1;
 	Map map2;
 	Map.Entry<Long, Short> map3;
 
+	private void test() {
+		System.out.println("test");
+	}
 
-	public static void main(String[] args) {
+	public void test1() {
+		System.out.println("test1");
+	}
+
+	private String test2() {
+		System.out.println("test2");
+		return "test2";
+	}
+
+
+	public static void main(String[] args) throws IllegalAccessException, InstantiationException, InvocationTargetException, NoSuchMethodException {
 		Field[] fields = ParameterizedBean.class.getDeclaredFields();
-//		Arrays.stream(fields).forEach(field -> {
-//					System.out.println(field.getName() + ":" + (field.getGenericType() instanceof ParameterizedType));
-//					System.out.println(field.getGenericType().getClass());
-//				}
-//		);
+		Field[] fields1 = FieldUtils.getAllFields(ParameterizedBean.class);
+		//		Arrays.stream(fields).forEach(field -> {
+		//					System.out.println(field.getName() + ":" + (field.getGenericType() instanceof ParameterizedType));
+		//					System.out.println(field.getGenericType().getClass());
+		//				}
+		//		);
+		Object object = ParameterizedBean.class.newInstance();
+		MethodUtils.invokeExactMethod(object, "test1");
+
+		Method method = ParameterizedBean.class.getDeclaredMethod("test");
+		method.setAccessible(true);
+		method.invoke(object, null);
 
 		for (Field f : fields) {
 			if (f.getGenericType() instanceof ParameterizedType) {
@@ -35,7 +57,7 @@ public class ParameterizedBean {
 				}
 
 
-//				System.out.println(parameterizedType.getClass());
+				//				System.out.println(parameterizedType.getClass());
 				Type[] types = parameterizedType.getActualTypeArguments();
 				for (Type t : types) {
 					System.out.println("类型" + t.getTypeName());
---
+++ src/main/java/com/caoc/test/parameterizedType/ParentFields.java
@@ -0,0 +1,11 @@
+package com.caoc.test.parameterizedType;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ParentFields {
+	private int i;
+	private Object object;
+}
