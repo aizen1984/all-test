ba737436a5b98f7d00b51db06ccf440d42eb413d
EthanStark <n_f.s@163.com>
2020-05-06 16:58:17 +0800

    ""

--- src/main/java/com/caoc/test/batchTest/BatchTest.java
+++ src/main/java/com/caoc/test/batchTest/BatchTest.java
@@ -1,16 +1,14 @@
 package com.caoc.test.batchTest;
 
 import com.caoc.test.TestApplication;
-import com.caoc.test.domain.TApplyInfo;
-import com.caoc.test.mapper.TApplyInfoMapper;
+import com.caoc.test.domain.TLendApply;
+import com.caoc.test.domain.TLendApplyExample;
+import com.caoc.test.mapper.TLendApplyMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -33,37 +31,60 @@ public class BatchTest {
 	private ApplicationContext applicationContext;
 
 	@Autowired
-	private TApplyInfoMapper tApplyInfoMapper;
+	private TLendApplyMapper tLendApplyMapper;
 
 
 	@Test
-	public void test() {
+	public void transactionTest() {
 
 		DataSourceTransactionManager dataSourceTransactionManager = applicationContext.getBean("transactionManager", DataSourceTransactionManager.class);
 		DefaultTransactionDefinition def = new DefaultTransactionDefinition();
 		def.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRES_NEW);
 		TransactionStatus status = dataSourceTransactionManager.getTransaction(def);
+		DefaultTransactionDefinition def2 = new DefaultTransactionDefinition();
+		TransactionStatus status2 = dataSourceTransactionManager.getTransaction(def2);
+		try {
+			tLendApplyMapper.deleteByExample(null);
+			doInsertAction();
+			dataSourceTransactionManager.commit(status);
+			doUpdateAction();
+			dataSourceTransactionManager.commit(status2);
+			doSelectAction();
+		} catch (Exception e) {
+			e.printStackTrace();
+			dataSourceTransactionManager.rollback(status);
+			dataSourceTransactionManager.rollback(status2);
+		}
+
+
+	}
+
+	private void doSelectAction() {
+		List<String> listOrderNos = Lists.newArrayList("order1", "order2", "order3");
+		System.out.println(tLendApplyMapper.selectBatchForPayOffByOrderNos(listOrderNos));
+	}
+
+
+
+	public void testLogic() {
 		//1 范围数据  假设构造1000条
 		List<String> list = IntStream.rangeClosed(1, 1000).mapToObj(t -> Integer.toString(t)).collect(Collectors.toList());
 		List<String> resultList = doQuery(list);
-//		list.forEach(t -> {
-//			resultList.add(t);
-//			if (resultList.size() == 50 || t.equals(list.get(list.size() - 1))) {
-//				List<String> needUpdate = Lists.newArrayList(resultList);
-//				List<String> needInsert = Lists.newArrayList(resultList);
-//				// 获取查询或者更新c
-//				needUpdate.retainAll(resultList);
-//				needInsert.removeAll(resultList);
-//				resultList.clear();
-//			}
-//
-//
-//		});
-		doInsertAction();
-		dataSourceTransactionManager.commit(status);
-		dataSourceTransactionManager.rollback(status);
+		list.forEach(t -> {
+			resultList.add(t);
+			if (resultList.size() == 50 || t.equals(list.get(list.size() - 1))) {
+				List<String> needUpdate = Lists.newArrayList(resultList);
+				List<String> needInsert = Lists.newArrayList(resultList);
+				// 获取查询或者更新c
+				needUpdate.retainAll(resultList);
+				needInsert.removeAll(resultList);
+				resultList.clear();
+			}
 
+
+		});
 	}
+
 
 	private List<String> doQuery(List<String> queryList) {
 		//处理查询范围 可以用mybatis来使用
@@ -73,20 +94,41 @@ public class BatchTest {
 	}
 
 	private void doUpdateAction() {
-
-
+		List<TLendApply> updateList = Lists.newArrayList();
+		TLendApply tLendApply = new TLendApply();
+		tLendApply.setOrderNo("order1");
+		updateList.add(tLendApply);
+		TLendApply tLendApply2 = new TLendApply();
+		tLendApply2.setOrderNo("order2");
+		updateList.add(tLendApply2);
+		tLendApplyMapper.updateBatchPayOffByOrderNo(updateList);
 	}
 
 	private void doInsertAction() {
-		IntStream.rangeClosed(1, 100).forEach(t -> {
-			TApplyInfo tApplyInfo = new TApplyInfo();
-			tApplyInfo.setUserName("caoc");
-			tApplyInfo.setUserCode("code_caoc");
-			tApplyInfo.setAge(11);
-			tApplyInfo.setCreateAt(new Date());
-			tApplyInfo.setUpdateAt(new Date());
-			tApplyInfoMapper.insert(tApplyInfo);
-		});
+		tLendApplyMapper.insertBatchPayOffSelective(IntStream.rangeClosed(1, 100).mapToObj(t -> {
+			TLendApply lendApply = new TLendApply();
+			lendApply.setLendApplyNo("i" + t);
+			lendApply.setUid("i" + t);
+			lendApply.setBizSerial("i" + t);
+			lendApply.setLendType("i" + t);
+			lendApply.setApplyTime(new Date());
+			lendApply.setApplyStatus("payoff2222");
+			lendApply.setSource("app");
+			lendApply.setUpdatedAt(new Date());
+			lendApply.setVersionNumber(t);
+			lendApply.setLendAmount(t);
+			lendApply.setCreatedAt(new Date());
+			lendApply.setOrderNo("order" + t);
+			try {
+				if (t == 99) {
+					throw new RuntimeException("故意的异常");
+				}
+
+			} catch (RuntimeException e) {
+				System.out.println("吃掉掉");
+			}
+			return lendApply;
+		}).collect(Collectors.toList()));
 
 
 	}
