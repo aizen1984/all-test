3b957edcc8a35b0e5c6a50aea10c7532fb7f9200
EthanStark <n_f.s@163.com>
2020-05-23 11:01:19 +0800

    ""

---
+++ src/main/java/com/caoc/test/modeMapper/ModMapperEntity.java
@@ -0,0 +1,15 @@
+package com.caoc.test.modeMapper;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class ModMapperEntity {
+	private String name;
+	private String code;
+	ModMapperEntity2 modMapperEntity2;
+
+}
